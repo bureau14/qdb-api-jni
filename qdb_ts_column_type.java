d50bc69983487c3e31e97fcf3e3243694a88268a
Leon Mergen <leon@solatis.com>
2017-09-07 15:49:04 +0200

    Expose column type enum

---
+++ qdb_ts_column_type.java
@@ -0,0 +1,7 @@
+package net.quasardb.qdb.jni;
+
+public final class qdb_ts_column_type {
+  public static final int uninitialized = -1;
+  public static final int double_ = 0;
+  public static final int blob = 1;
+}
