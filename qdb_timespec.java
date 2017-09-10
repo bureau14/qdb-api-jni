feb68d932b66e4ce51e5ec6d895b325ba7250fec
Leon Mergen <leon@solatis.com>
2017-09-10 08:52:16 +0200

    Fix compile error

--- qdb_timespec.java
+++ qdb_timespec.java
@@ -4,7 +4,7 @@ public final class qdb_timespec {
   long tv_sec;
   long tv_nsec;
 
-  public qdb_ts_column_info(long tv_sec, long tv_nsec){
+  public qdb_timespec(long tv_sec, long tv_nsec){
     this.tv_sec = tv_sec;
     this.tv_nsec = tv_nsec;
   }
