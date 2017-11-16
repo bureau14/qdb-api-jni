5d59bed422bdd3f15c5fa6ad10f80b4330e195d8
Leon Mergen <leon@solatis.com>
2017-11-16 13:20:42 +0100

    Gets rid of some debugging info

--- qdb_cluster_security_options.java
+++ qdb_cluster_security_options.java
@@ -9,9 +9,6 @@ public final class qdb_cluster_security_options {
   public qdb_cluster_security_options(String user_name,
                                       String user_private_key,
                                       String cluster_public_key) {
-    System.out.println("1 user_name = " + user_name);
-    System.out.println("1 user_private_key = " + user_private_key);
-    System.out.println("1 cluster_public_key = " + cluster_public_key);
     this.user_name = user_name;
     this.user_private_key = user_private_key;
     this.cluster_public_key = cluster_public_key;
