2c98e38bdb1ebbd0109bba061133f5d546bc8900
Leon Mergen <leon@solatis.com>
2017-11-17 15:02:12 +0100

    Implements serialization for ts values

--- src/main/net/quasardb/qdb/timeseries/QdbTimeSeriesValue.java
+++ src/main/net/quasardb/qdb/timeseries/QdbTimeSeriesValue.java
@@ -1,12 +1,13 @@
 package net.quasardb.qdb;
 
+import java.io.*;
 import java.nio.ByteBuffer;
 import net.quasardb.qdb.jni.*;
 
 /**
  * Represents a timeseries table.
  */
-public class QdbTimeSeriesValue {
+public class QdbTimeSeriesValue implements Serializable {
 
     protected Type type;
     protected double doubleValue;
@@ -21,57 +22,74 @@ public class QdbTimeSeriesValue {
         Type(int type) {
             this.value = type;
         }
+
+        protected static Type fromInt(int type) {
+            switch(type) {
+            case qdb_ts_column_type.double_:
+                return Type.DOUBLE;
+
+            case qdb_ts_column_type.blob:
+                return Type.BLOB;
+            }
+
+            return Type.UNINITIALIZED;
+        }
+    }
+
+    protected QdbTimeSeriesValue(Type type) {
+        this.type = type;
     }
 
     /**
      * Represents a double value.
      */
-    public static class Double extends QdbTimeSeriesValue {
-        public Double(double value) {
-            this.type = Type.DOUBLE;
-            this.doubleValue = value;
-        }
+    public static QdbTimeSeriesValue createDouble(double value) {
+        QdbTimeSeriesValue val = new QdbTimeSeriesValue(Type.DOUBLE);
+        val.doubleValue = value;
+        return val;
     }
 
     /**
      * Represents a blob value. Warning: assumes byte array will stay in memory for
      * as long as this object lives.
      */
-    public static class Blob extends QdbTimeSeriesValue {
-        public Blob(byte[] value) {
-            this.type = Type.BLOB;
-            this.blobValue = ByteBuffer.wrap(value);
-        }
+    public static QdbTimeSeriesValue createBlob(byte[] value) {
+        QdbTimeSeriesValue val = new QdbTimeSeriesValue(Type.BLOB);
+        val.blobValue = ByteBuffer.wrap(value);
+        return val;
+    }
 
-        public Blob(ByteBuffer value) {
-            this.type = Type.BLOB;
-            this.blobValue = value.duplicate();
-        }
+    public static QdbTimeSeriesValue createBlob(ByteBuffer value) {
+        QdbTimeSeriesValue val = new QdbTimeSeriesValue(Type.BLOB);
+        val.blobValue = value.duplicate();
+        return val;
     }
 
     /**
      * Represents a safe blob value that copies the byte array.
      */
-    public static class SafeBlob extends QdbTimeSeriesValue {
-        public SafeBlob(byte[] value) {
-            this.type = Type.BLOB;
+    public static QdbTimeSeriesValue createSafeBlob(byte[] value) {
+        QdbTimeSeriesValue val = new QdbTimeSeriesValue(Type.BLOB);
 
-            int size = value.length;
-            this.blobValue = ByteBuffer.allocateDirect(size);
-            this.blobValue.put(value, 0, size);
-            this.blobValue.rewind();
-        }
+        int size = value.length;
+        val.blobValue = ByteBuffer.allocateDirect(size);
+        val.blobValue.put(value, 0, size);
+        val.blobValue.rewind();
 
-        public SafeBlob(ByteBuffer value) {
-            this.type = Type.BLOB;
+        return val;
+    }
 
-            int size = value.capacity();
-            this.blobValue = ByteBuffer.allocateDirect(size);
-            this.blobValue.put(value);
-            this.blobValue.rewind();
+    public static QdbTimeSeriesValue createSafeBlob(ByteBuffer value) {
+        QdbTimeSeriesValue val = new QdbTimeSeriesValue(Type.BLOB);
 
-            value.rewind();
-        }
+        int size = value.capacity();
+        val.blobValue = ByteBuffer.allocateDirect(size);
+        val.blobValue.put(value);
+        val.blobValue.rewind();
+
+        value.rewind();
+
+        return val;
     }
 
     @Override
@@ -93,6 +111,65 @@ public class QdbTimeSeriesValue {
 
         return false;
     }
+
+    private void writeObject(java.io.ObjectOutputStream stream)
+        throws IOException {
+        stream.writeInt(this.type.value);
+
+        switch (this.type) {
+        case DOUBLE:
+            stream.writeDouble(this.doubleValue);
+            break;
+
+        case BLOB:
+            writeBlobValue(stream, this.blobValue);
+            break;
+        }
+    }
+
+    private static void writeBlobValue(java.io.ObjectOutputStream stream, ByteBuffer value)
+        throws IOException {
+        int size = value.capacity();
+
+        byte[] buffer = new byte[size];
+        value.get(buffer, 0, size);
+
+        stream.writeInt(value.capacity());
+        stream.write(buffer, 0, size);
+
+        value.rewind();
+    }
+
+
+    private void readObject(java.io.ObjectInputStream stream)
+        throws IOException, ClassNotFoundException {
+        this.type = Type.fromInt(stream.readInt());
+
+        switch (this.type) {
+        case DOUBLE:
+            this.doubleValue = stream.readDouble();
+            break;
+
+        case BLOB:
+            this.blobValue = readBlobValue(stream);
+            break;
+        }
+    }
+
+    private ByteBuffer readBlobValue(java.io.ObjectInputStream stream)
+        throws IOException, ClassNotFoundException {
+        int size = stream.readInt();
+
+        byte[] buffer = new byte[size];
+        stream.read(buffer, 0, size);
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(size);
+        bb.put(buffer, 0, size);
+        bb.rewind();
+
+        return bb.duplicate();
+    }
+
 
     public Type getType() {
         return this.type;
