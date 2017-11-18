be9522d0bbf6caca4dc512316b73041dfe245384
Leon Mergen <leon@solatis.com>
2017-11-18 13:43:13 +0100

    Provides easier timespec constructor for rows

--- src/main/net/quasardb/qdb/timeseries/QdbTimeSeriesRow.java
+++ src/main/net/quasardb/qdb/timeseries/QdbTimeSeriesRow.java
@@ -2,6 +2,9 @@ package net.quasardb.qdb;
 
 import java.io.IOException;
 import java.nio.channels.SeekableByteChannel;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import net.quasardb.qdb.jni.*;
 import java.util.*;
 
@@ -17,6 +20,16 @@ public final class QdbTimeSeriesRow {
         this.timestamp = timestamp;
         this.values = values;
     }
+
+    public QdbTimeSeriesRow(LocalDateTime timestamp, QdbTimeSeriesValue[] values) {
+        this(new QdbTimespec(timestamp), values);
+    }
+
+    public QdbTimeSeriesRow(Timestamp timestamp, QdbTimeSeriesValue[] values) {
+        this(new QdbTimespec(timestamp), values);
+    }
+
+
 
     public QdbTimespec getTimestamp() {
         return this.timestamp;
