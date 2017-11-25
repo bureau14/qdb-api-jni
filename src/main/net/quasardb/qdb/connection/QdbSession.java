2f684d6dba95def6e64efb08437f97b38bc291dc
Leon Mergen <leon@solatis.com>
2017-11-25 13:39:24 +0100

    Performs work on connection pooling

--- src/main/net/quasardb/qdb/connection/QdbSession.java
+++ src/main/net/quasardb/qdb/connection/QdbSession.java
@@ -1,5 +1,7 @@
 package net.quasardb.qdb;
 
+import java.io.Serializable;
+import net.quasardb.qdb.*;
 import net.quasardb.qdb.jni.*;
 import java.nio.ByteBuffer;
 
@@ -8,9 +10,56 @@ import java.nio.ByteBuffer;
  */
 class QdbSession {
     private transient long handle;
+    private qdb_cluster_security_options securityOptions;
 
+    public static class SecurityOptions implements Serializable {
+        protected String userName;
+        protected String userPrivateKey;
+        protected String clusterPublicKey;
+
+        public SecurityOptions (String userName,
+                                String userPrivateKey,
+                                String clusterPublicKey) {
+            this.userName = userName;
+            this.userPrivateKey = userPrivateKey;
+            this.clusterPublicKey = clusterPublicKey;
+        }
+
+        static qdb_cluster_security_options toNative(SecurityOptions options) {
+          return new qdb_cluster_security_options(options.userName,
+                                                  options.userPrivateKey,
+                                                  options.clusterPublicKey);
+        }
+    }
+
+    /**
+     * Initialize a new QdbSession without security settings. Connections to the
+     * QuasarDB cluster will be insecure and unauthenticated.
+     */
     public QdbSession() {
         handle = qdb.open_tcp();
+    }
+
+    /**
+     * Initialize a new QdbSession with security settings. Connections to the
+     * QuasarDB will use a secure connection and will be authenticated.
+     */
+    public QdbSession(SecurityOptions securityOptions) {
+        this.securityOptions = SecurityOptions.toNative(securityOptions);
+        handle = qdb.open_tcp();
+    }
+
+    /**
+     * Establishes a connection
+     */
+    public void connect(String uri) {
+        if (this.securityOptions == null) {
+            int err = qdb.connect(this.handle, uri);
+            QdbExceptionFactory.throwIfError(err);
+        } else {
+            int err = qdb.secure_connect(this.handle, uri, securityOptions);
+            QdbExceptionFactory.throwIfError(err);
+        }
     }
 
     public void close() {
--- src/main/net/quasardb/qdb/connection/QdbSessionPool.java
+++ src/main/net/quasardb/qdb/connection/QdbSessionPool.java
@@ -8,22 +8,28 @@ import net.quasardb.qdb.*;
  * Provides an object pool which allows for sharing and re-use of quasardb
  * connections over a period of time.
  */
-class QdbSessionPool {
+class QdbSessionPool implements AutoCloseable {
 
     /**
      * Keeps track of all the currently allocated sessions.
      */
-    private Pool<PoolableSession> pool;
+    private BlazePool<PoolableSession> pool;
 
     /**
      * A QdbSession that provides the interface necessary for connection pool
      * management. The user of this class must be careful to call release()
      * after they are done with the class.
      */
-    private static class PoolableSession extends QdbSession implements Poolable {
+    public static class PoolableSession extends QdbSession implements Poolable {
         private final Slot slot;
 
-        public PoolableSession(Slot slot) {
+        public PoolableSession( Slot slot) {
+            super();
+            this.slot = slot;
+        }
+
+        public PoolableSession(QdbSession.SecurityOptions securityOptions, Slot slot) {
+            super(securityOptions);
             this.slot = slot;
         }
 
@@ -33,9 +39,16 @@ class QdbSessionPool {
     }
 
     private static class SessionAllocator implements Allocator<PoolableSession> {
+        private String uri;
+
+        SessionAllocator(String uri) {
+            this.uri = uri;
+        }
 
         public PoolableSession allocate(Slot slot) {
-            return new PoolableSession(slot);
+            PoolableSession s = new PoolableSession(slot);
+            s.connect(uri);
+            return s;
         }
 
         public void deallocate(PoolableSession s) {
@@ -43,8 +56,34 @@ class QdbSessionPool {
         }
     }
 
-    public QdbSessionPool() {
-        Config<PoolableSession> config = new Config<PoolableSession>().setAllocator(new SessionAllocator());
+    private static class SecureSessionAllocator implements Allocator<PoolableSession> {
+        private String uri;
+        private QdbSession.SecurityOptions securityOptions;
+
+        SecureSessionAllocator(String uri, QdbSession.SecurityOptions securityOptions) {
+            this.uri = uri;
+            this.securityOptions = securityOptions;
+        }
+
+        public PoolableSession allocate(Slot slot) {
+            PoolableSession s = new PoolableSession(securityOptions, slot);
+            s.connect(uri);
+            return s;
+        }
+
+        public void deallocate(PoolableSession s) {
+            s.close();
+        }
+    }
+
+    public QdbSessionPool(String uri) {
+        Config<PoolableSession> config = new Config<PoolableSession>().setAllocator(new SessionAllocator(uri));
+        this.pool = new BlazePool<PoolableSession>(config);
+    }
+
+    public QdbSessionPool(String uri, QdbSession.SecurityOptions securityOptions) {
+        Config<PoolableSession> config =
+            new Config<PoolableSession>().setAllocator(new SecureSessionAllocator(uri, securityOptions));
         this.pool = new BlazePool<PoolableSession>(config);
     }
 
@@ -52,15 +91,33 @@ class QdbSessionPool {
      * Acquire a new QdbSession with a default timeout of 1 second. Ensure to call
      * release() after being finished using a session.
      */
-    public PoolableSession claim() throws InterruptedException  {
-        return this.claim(new Timeout(1, TimeUnit.SECONDS));
+    public PoolableSession claim()  {
+        return this.claim(new Timeout(15, TimeUnit.SECONDS));
     }
 
     /**
      * Acquire a new QdbSession with a customizeable timeout. Ensure to call release()
      * after being finished using a session.
+     *
+     * @throws QdbConnectionException If a session could not be acquired from the pool.
      */
-    public PoolableSession claim(Timeout timeout) throws InterruptedException {
-        return this.pool.claim(timeout);
+    public PoolableSession claim(Timeout timeout) {
+
+        try {
+            PoolableSession s = this.pool.claim(timeout);
+            s.throwIfClosed();
+
+            if (s == null) {
+                throw new QdbConnectionException("A timeout occurred while acquiring a connection from the pool.");
+            }
+
+            return s;
+        } catch (InterruptedException e) {
+            throw new QdbConnectionException("Thread was interrupted while acquiring a connection from the pool.");
+        }
+    }
+
+    public void close() {
+        this.pool.shutdown();
     }
 }
