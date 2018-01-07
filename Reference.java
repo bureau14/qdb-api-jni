d95e26ac36eda8e49b4754636ea8fcaea5fa6cc1
Leon Mergen <leon@solatis.com>
2018-01-07 16:43:18 +0100

    Add some helper methods to our reference

--- Reference.java
+++ Reference.java
@@ -1,3 +1,17 @@
 package net.quasardb.qdb.jni;
 
-public class Reference<T> { public T value; }
+public class Reference<T> {
+    public T value;
+
+    public void clear() {
+        this.value = null;
+    }
+
+    public T get() {
+        return this.value;
+    }
+
+    public boolean isEmpty() {
+        return this.value == null;
+    }
+}
