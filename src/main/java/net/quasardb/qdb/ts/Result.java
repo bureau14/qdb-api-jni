724bae83ead8352ea971e2436dfa7cf51a3b63a4
Leon Mergen <leon@solatis.com>
2018-01-21 11:37:11 +0100

    Make result fields public for easy inspection

--- src/main/java/net/quasardb/qdb/ts/Result.java
+++ src/main/java/net/quasardb/qdb/ts/Result.java
@@ -10,9 +10,9 @@ public final class Result {
     public Table[] tables;
 
     public static class Table {
-        protected String name;
-        protected String[] columns;
-        protected Value[][] rows;
+        public String name;
+        public String[] columns;
+        public Value[][] rows;
 
         public String toString() {
             return "Table (name: " + this.name + ", columns: " + Arrays.toString(this.columns) + ", rows: " + Arrays.deepToString(this.rows) + ")";
