c37ae9f15110da2902e7a238b80765130a40dc78
Leon Mergen <leon@solatis.com>
2018-01-27 14:00:25 +0100

    Hexdump appears to be broken under win32, let's not use it for now

--- src/main/c++/net/quasardb/qdb/jni/debug.h
+++ src/main/c++/net/quasardb/qdb/jni/debug.h
@@ -3,21 +3,22 @@
 #include <string>
 
 namespace qdb {
-  namespace jni {
+    namespace jni {
 
-    class env;
+        class env;
 
-    class debug {
-    public:
-      static void
-      hexdump(env & env, void const * buf, size_t len);
+        class debug {
+        public:
+            static void
+            println(env & env, std::string const & msg);
 
-      static void
-      println(env & env, std::string const & msg);
+            static void
+            println(env & env, char const * msg);
 
-      static void
-      println(env & env, char const * msg);
+        private:
+            static void
+            hexdump(env & env, void const * buf, size_t len);
 
+        };
     };
-  };
 };
