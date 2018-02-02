f2a1d3a652adc96ceed272afe39a90bfb4c66a52
Leon Mergen <leon@solatis.com>
2018-02-02 16:54:30 +0100

    Improves function scopes and javadocs

--- src/main/java/net/quasardb/qdb/ts/Column.java
+++ src/main/java/net/quasardb/qdb/ts/Column.java
@@ -6,22 +6,34 @@ import java.util.stream.Collectors;
 
 import net.quasardb.qdb.jni.*;
 
+/**
+ * Holds information about a single column.
+ */
 public class Column {
     protected String name;
     protected Value.Type type;
 
+    /**
+     * A blob column.
+     */
     public static class Blob extends Column {
         public Blob(String name) {
             super(name, Value.Type.BLOB);
         }
     }
 
+    /**
+     * A double precision column.
+     */
     public static class Double extends Column {
         public Double(String name) {
             super(name, Value.Type.DOUBLE);
         }
     }
 
+    /**
+     * A 64 bit integer column.
+     */
     public static class Int64 extends Column {
         public Int64(String name) {
             super(name, Value.Type.INT64);
@@ -29,25 +41,37 @@ public class Column {
     }
 
 
+    /**
+     * A timestamp column.
+     */
     public static class Timestamp extends Column {
         public Timestamp(String name) {
             super(name, Value.Type.TIMESTAMP);
         }
     }
 
+    /**
+     * Create a column with a certain type.
+     */
     public Column(String name, Value.Type type) {
         this.name = name;
         this.type = type;
     }
 
-    public Column(String name, int type) {
+    protected Column(String name, int type) {
         this(name, Value.Type.fromInt(type));
     }
 
+    /**
+     * Provides access to the column's name.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Provides access to the column's type.
+     */
     public Value.Type getType() {
         return this.type;
     }
--- src/main/java/net/quasardb/qdb/ts/FilteredRange.java
+++ src/main/java/net/quasardb/qdb/ts/FilteredRange.java
@@ -2,24 +2,35 @@ package net.quasardb.qdb.ts;
 
 import net.quasardb.qdb.jni.*;
 
+/**
+ * Represents a time range with additional filters applied.
+ *
+ * Currently serves as a placeholder class, as no filters are supported yet.
+ */
 public final class FilteredRange {
     TimeRange range;
     qdb_ts_filter filter;
 
+    /**
+     * Wraps a {@link TimeRange} without applying any filters.
+     */
     public FilteredRange(TimeRange range){
         this(range, new qdb_ts_no_filter());
     }
 
-    public FilteredRange(TimeRange range, qdb_ts_filter filter) {
+    protected FilteredRange(TimeRange range, qdb_ts_filter filter) {
         this.range = range;
         this.filter = filter;
     }
 
+    /**
+     * Access to the underlying TimeRange.
+     */
     public TimeRange getRange() {
         return this.range;
     }
 
-    public qdb_ts_filter getFilter() {
+    protected qdb_ts_filter getFilter() {
         return this.filter;
     }
 
