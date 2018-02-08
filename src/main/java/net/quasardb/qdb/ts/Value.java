5c971333c4e2b86a9e3057835a3fe4246388780d
Leon Mergen <leon@solatis.com>
2018-02-08 15:01:33 +0100

    Fixes private/public variable scoping

--- src/main/java/net/quasardb/qdb/ts/Value.java
+++ src/main/java/net/quasardb/qdb/ts/Value.java
@@ -13,13 +13,11 @@ import net.quasardb.qdb.exception.IncompatibleTypeException;
  */
 public class Value implements Serializable {
 
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-    public Type type;
-    public long int64Value;
-    public double doubleValue;
-    public Timespec timestampValue;
-    public ByteBuffer blobValue;
+    Type type;
+    long int64Value;
+    double doubleValue;
+    Timespec timestampValue;
+    ByteBuffer blobValue;
 
     public enum Type {
         UNINITIALIZED(qdb_ts_column_type.uninitialized),
@@ -189,17 +187,6 @@ public class Value implements Serializable {
 
         return val;
     }
-
-    private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
 
     @Override
     public boolean equals(Object obj) {
