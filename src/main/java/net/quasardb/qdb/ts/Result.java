f15e0cb0a6a3e2d95bb479db1b4935de62b8de3c
Leon Mergen <leon@solatis.com>
2018-02-11 12:07:35 +0100

    Implements stream based access to query results

--- src/main/java/net/quasardb/qdb/ts/Result.java
+++ src/main/java/net/quasardb/qdb/ts/Result.java
@@ -15,6 +15,44 @@ public final class Result {
     public Table[] tables;
 
     /**
+     * Represents a single Result row.
+     */
+    public static class Row {
+        /**
+         * The name of the table the Row is from.
+         */
+        public String tableName;
+
+
+        /**
+         * The column identifiers of the Table the Row is from.
+         */
+        public String[] columns;
+
+        /**
+         * The values of this row. Contains exactly one value per column.
+         */
+        public Value[] values;
+
+        /**
+         * Constructor.
+         *
+         * @param tableName The name of the Table this Row is from.
+         * @param columns The column identifiers of the Table this Row is from.
+         * @param values The values of this row.
+         **/
+        protected Row(String tableName, String[] columns, Value[] values) {
+            this.tableName = tableName;
+            this.columns = columns;
+            this.values = values;
+        }
+
+        public String toString() {
+            return "Row (tableName: " + this.tableName + ", columns: " + Arrays.toString(this.columns) + ", values: " + Arrays.toString(this.values) + ")";
+        }
+    }
+
+    /**
      * Represents a Table that is part of the Query result.
      */
     public static class Table {
@@ -32,6 +70,19 @@ public final class Result {
          * A array of all rows. Contains exactly one value per column.
          */
         public Value[][] rows;
+
+        /**
+         * Flattens this Table by coercing the columns/values into Rows.
+         */
+        public Row[] flatten() {
+            Row[] result = new Row[this.rows.length];
+
+            for (int i = 0; i < this.rows.length; ++i) {
+                result[i] = new Row(this.name, this.columns, this.rows[i]);
+            }
+
+            return result;
+        }
 
         public String toString() {
             return "Table (name: " + this.name + ", columns: " + Arrays.toString(this.columns) + ", rows: " + Arrays.deepToString(this.rows) + ")";
@@ -63,7 +114,10 @@ public final class Result {
     /**
      * Provides stream-based access.
      */
-    public Stream<Table> stream() {
-        return Arrays.stream(this.tables);
+    public Stream<Row> stream() {
+        return
+            Arrays.stream(this.tables)
+            .map(Table::flatten)
+            .flatMap(Arrays::stream);
     }
 }
