6dae6679610dad6beda20540297553ba797bd32f
Marek Kurdej <marek@quasardb.net>
2019-05-07 15:22:26 +0200

    Bump version to 3.4.0.0-nightly.0.

--- native_jar.cmake
+++ native_jar.cmake
@@ -10,7 +10,7 @@ else()
     string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM)
 endif()
 
-set(NATIVE_JAR_FILE "${CMAKE_BINARY_DIR}/jni-3.3.0-SNAPSHOT-${SYSTEM}-${ARCH}.jar")
+set(NATIVE_JAR_FILE "${CMAKE_BINARY_DIR}/jni-3.4.0-SNAPSHOT-${SYSTEM}-${ARCH}.jar")
 
 # Quasardb C API
 if (WIN32)
