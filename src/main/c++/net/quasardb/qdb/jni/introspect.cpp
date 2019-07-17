c1c21c62145c0fc3f1234cbd429fbc6083951347
Leon Mergen <leon@solatis.com>
2019-07-17 11:08:57 +0200

    Add debug information

--- src/main/c++/net/quasardb/qdb/jni/introspect.cpp
+++ src/main/c++/net/quasardb/qdb/jni/introspect.cpp
@@ -4,6 +4,10 @@
 /* static */ jclass
 qdb::jni::introspect::lookup_class(env & env, char const * alias) {
   jclass c = env.instance().FindClass(alias);
+  if (c == NULL) {
+    fprintf(stderr, "*** Unable to find class with signature: %s\n", alias);
+    fflush(stderr);
+  }
   assert(c != NULL);
   return c;
 }
@@ -12,6 +16,12 @@ qdb::jni::introspect::lookup_class(env & env, char const * alias) {
 qdb::jni::introspect::lookup_field(env & env, jclass objectClass, char const * alias, char const * signature) {
   assert(objectClass != NULL);
   jfieldID field = env.instance().GetFieldID(objectClass, alias, signature);
+
+  if (field == NULL) {
+    fprintf(stderr, "*** Unable to find field with signature: %s\n", alias);
+    fflush(stderr);
+  }
+
   assert(field != NULL);
   return field;
 }
@@ -20,6 +30,12 @@ qdb::jni::introspect::lookup_field(env & env, jclass objectClass, char const * a
 qdb::jni::introspect::lookup_static_field(env & env, jclass objectClass, char const * alias, char const * signature) {
   assert(objectClass != NULL);
   jfieldID field = env.instance().GetStaticFieldID(objectClass, alias, signature);
+
+  if (field == NULL) {
+    fprintf(stderr, "*** Unable to find static field with signature: %s\n", alias);
+    fflush(stderr);
+  }
+
   assert(field != NULL);
   return field;
 }
@@ -28,6 +44,12 @@ qdb::jni::introspect::lookup_static_field(env & env, jclass objectClass, char co
 qdb::jni::introspect::lookup_method(env & env, jclass objectClass, char const * alias, char const * signature) {
   assert(objectClass != NULL);
   jmethodID method = env.instance().GetMethodID(objectClass, alias, signature);
+
+  if (method == NULL) {
+    fprintf(stderr, "*** Unable to find method with signature: %s\n", alias);
+    fflush(stderr);
+  }
+
   assert(method != NULL);
   return method;
 }
@@ -36,6 +58,12 @@ qdb::jni::introspect::lookup_method(env & env, jclass objectClass, char const * 
 qdb::jni::introspect::lookup_static_method(env & env, jclass objectClass, char const * alias, char const * signature) {
   assert(objectClass != NULL);
   jmethodID method = env.instance().GetStaticMethodID(objectClass, alias, signature);
+
+  if (method == NULL) {
+    fprintf(stderr, "*** Unable to find method with signature: %s\n", alias);
+    fflush(stderr);
+  }
+
   assert(method != NULL);
   return method;
 }
