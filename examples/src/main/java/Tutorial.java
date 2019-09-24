ef0d71a71bed318d11a19174efd5ac1aa391cbd4
Leon Mergen <leon@solatis.com>
2019-09-24 11:24:17 +0200

    Add 'create table' example

--- examples/src/main/java/Tutorial.java
+++ examples/src/main/java/Tutorial.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 // import-start
 import net.quasardb.qdb.*;
+import net.quasardb.qdb.ts.*;
 import net.quasardb.qdb.jni.*;
 import net.quasardb.qdb.exception.*;
 // import-end
@@ -9,17 +10,8 @@ import net.quasardb.qdb.exception.*;
 public class Tutorial {
 
     public static void main(String[] args) throws IOException {
-
-        // connect-start
-        Session c;
-
-        try {
-            c = Session.connect("qdb://127.0.0.1:28360");
-        } catch (ConnectionRefusedException ex) {
-            System.err.println("Failed to connect to cluster, make sure server is running!");
-            System.exit(1);
-        }
-        // connect-end
+        Session c = Tutorial.connect();
+        Table t = Tutorial.createTable(c);
     }
 
 
@@ -39,6 +31,39 @@ public class Tutorial {
         }
         // secure-connect-end
 
+    }
+
+    private static Session connect() throws ConnectionRefusedException {
+        // connect-start
+        Session c;
+
+        try {
+            c = Session.connect("qdb://127.0.0.1:28360");
+        } catch (ConnectionRefusedException ex) {
+            System.err.println("Failed to connect to cluster, make sure server is running!");
+            throw ex;
+        }
+
+        // connect-end
+
+        return c;
+    }
+
+    private static Table createTable(Session c) {
+        // create-table-start
+
+        Column[] definitions = {
+            new Column.Double ("open"),
+            new Column.Double ("close"),
+            new Column.Int64 ("volume")
+        };
+
+        // This will return a reference to the newly created Table
+        Table t = Table.create(c, "stocks", definitions);
+
+        // create-table-end
+
+        return t;
     }
 
 }
