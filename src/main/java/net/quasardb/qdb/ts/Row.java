565645a8a7852616a7a6fbf482c932a2cc50cf76
Leon Mergen <leon@solatis.com>
2019-10-21 15:08:21 +0200

    QDB-2779 Remove deprecated functionality

--- src/main/java/net/quasardb/qdb/ts/Row.java
+++ src/main/java/net/quasardb/qdb/ts/Row.java
@@ -15,11 +15,11 @@ import java.util.*;
  */
 public class Row implements Serializable {
 
-    protected Timespec timestamp;
     protected Value[] values;
 
     /**
-     * @brief Row without timestamp
+     * Row without timestamp
+     *
      * @param values All values for this row.
      *
      * When querying data from the database, a row might not have a timestamp. This
@@ -27,46 +27,6 @@ public class Row implements Serializable {
      */
     public Row(Value[] values) {
         this.values = values;
-    }
-
-    /**
-     * @brief Row with timestamp
-     * @param timestamp The Valid Time for the row. This timestamp will be the primary index
-     *                  that quasardb stores this row under.
-     * @param values All values for this row.
-     */
-    public Row(Timespec timestamp, Value[] values) {
-        this.timestamp = timestamp;
-        this.values = values;
-    }
-
-    /**
-     * @brief Row with timestamp
-     * @param timestamp The Valid Time for the row. This timestamp will be the primary index
-     *                  that quasardb stores this row under.
-     * @param values All values for this row.
-     */
-    public Row(LocalDateTime timestamp, Value[] values) {
-        this(new Timespec(timestamp), values);
-    }
-
-    /**
-     * @brief Row with timestamp
-     * @param timestamp The Valid Time for the row. This timestamp will be the primary index
-     *                  that quasardb stores this row under.
-     * @param values All values for this row.
-     */
-    public Row(Timestamp timestamp, Value[] values) {
-        this(new Timespec(timestamp), values);
-    }
-
-    /**
-     * @brief Access to the timestamp of this row.
-     * @returns The timestamp, or null if this row does not have a timestamp associated (in case
-     *          of a query result, for example).
-     */
-    public Timespec getTimestamp() {
-        return this.timestamp;
     }
 
     /**
@@ -81,16 +41,10 @@ public class Row implements Serializable {
         if (!(obj instanceof Row)) return false;
         Row rhs = (Row)obj;
 
-        if (this.timestamp != null) {
-            if (rhs.getTimestamp() != null) return false;
-
-            if (this.getTimestamp().equals(rhs.getTimestamp()) == false) return false;
-        }
-
         return Arrays.equals(this.getValues(), rhs.getValues());
     }
 
     public String toString() {
-        return "Row (timestamp: " + (this.timestamp != null ? this.timestamp.toString() : "NULL") + ", values = " + Arrays.toString(this.values) + ")";
+        return "Row (values = " + Arrays.toString(this.values) + ")";
     }
 }
