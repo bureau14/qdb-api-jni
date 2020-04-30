b7a8171f197838360b0207173ea7a7b62c6fec2b
Leon Mergen <leon@solatis.com>
2020-04-30 09:48:48 +0200

    Implement byte array guard
    Automatically release byte array memory

---
+++ src/main/c++/net/quasardb/qdb/jni/byte_array.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <jni.h>
+
+#include "guard/byte_array.h"
+#include "introspect.h"
+
+namespace qdb
+{
+namespace jni
+{
+
+class env;
+
+/**
+ * Wraps around all JNI operations that relate to byte array management.
+ */
+class byte_array
+{
+  public:
+
+    /**
+     * Get a reference to the underlying byte array.
+     */
+    static jni::guard::byte_array
+    get_bytes(qdb::jni::env & env, jbyteArray bb) {
+      return guard::byte_array(env, bb);
+    }
+};
+}; // namespace jni
+}; // namespace qdb
