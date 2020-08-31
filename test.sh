9d526d19f5c3117c12a25968c89aefc4480810fc
Leon Mergen <leon@solatis.com>
2020-08-31 10:59:24 +0200

    Add some documentation to test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This is just a utility file for local development, it is not intended for
+# any production/CI usage.
+
 # Generate JNI header files if this is first run
 [ ! -d "target/" ] && mvn compile
 
@@ -11,4 +14,4 @@ mkdir build || true && \
     # mvn -X test
     # mvn  -X '-Dtest=WriterExtraTablesBenchmarkTest*' test
     # mvn   -X  '-Dtest=TableTest*' test
-    mvn -X '-Dtest=WriterTest#canAddExtraTable' test
+    mvn -X '-Dtest=WriterTest' test
