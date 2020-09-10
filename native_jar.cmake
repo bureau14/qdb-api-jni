ce294c2fe883f2ff801de3e6aafa397dca914810
vianney <vianney.p@outlook.com>
2020-09-10 13:49:28 +0200

    Bump version to 3.9.3.

--- native_jar.cmake
+++ native_jar.cmake
@@ -10,7 +10,7 @@ else()
     string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM)
 endif()
 
-set(NATIVE_JAR_FILE "${CMAKE_SOURCE_DIR}/target/jni-3.9.3-SNAPSHOT-${SYSTEM}-${ARCH}.jar")
+set(NATIVE_JAR_FILE "${CMAKE_SOURCE_DIR}/target/jni-3.9.3-${SYSTEM}-${ARCH}.jar")
 
 # Quasardb C API
 if (WIN32)
