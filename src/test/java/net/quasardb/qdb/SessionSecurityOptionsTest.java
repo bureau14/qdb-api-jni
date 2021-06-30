accf19b7f403eaf74ed3f75b46f9cbf35d52b31d
Leon Mergen <leon@solatis.com>
2021-06-30 10:01:34 +0200

    Fix backwards compatibility with java 8

--- src/test/java/net/quasardb/qdb/SessionSecurityOptionsTest.java
+++ src/test/java/net/quasardb/qdb/SessionSecurityOptionsTest.java
@@ -35,4 +35,26 @@ public class SessionSecurityOptionsTest {
         assertEquals(so.clusterPublicKey, "Pm6Y4ggcD1FhFAviA6EQSUtv/5qjT/IFIdrKNeCJtiyM=");
     }
 
+
+    @Test
+    public void canReadRelativePaths () throws IOException {
+        Session.SecurityOptions so =
+            Session.SecurityOptions.ofFiles("./user_private.key",
+                                            "./cluster_public.key");
+
+        assertEquals(so.userName, "test-user");
+        assertEquals(so.userPrivateKey, "S0huLfhqH+046Hy9UYAY6r4N2/P7Hbeq94FyAoQSlxuA=");
+        assertEquals(so.clusterPublicKey, "Pm6Y4ggcD1FhFAviA6EQSUtv/5qjT/IFIdrKNeCJtiyM=");
+    }
+
+    @Test
+    public void canReadComplexPaths () throws IOException {
+        Session.SecurityOptions so =
+            Session.SecurityOptions.ofFiles("../qdb-api-jni/user_private.key",
+                                            "../qdb-api-jni/cluster_public.key");
+
+        assertEquals(so.userName, "test-user");
+        assertEquals(so.userPrivateKey, "S0huLfhqH+046Hy9UYAY6r4N2/P7Hbeq94FyAoQSlxuA=");
+        assertEquals(so.clusterPublicKey, "Pm6Y4ggcD1FhFAviA6EQSUtv/5qjT/IFIdrKNeCJtiyM=");
+    }
 }
