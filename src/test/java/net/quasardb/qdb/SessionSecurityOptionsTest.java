00b6ac785dfb2d8ee57ff32d93e1256a4bfce2dc
Leon Mergen <leon@solatis.com>
2021-06-30 10:09:25 +0200

    Fix test

--- src/test/java/net/quasardb/qdb/SessionSecurityOptionsTest.java
+++ src/test/java/net/quasardb/qdb/SessionSecurityOptionsTest.java
@@ -46,15 +46,4 @@ public class SessionSecurityOptionsTest {
         assertEquals(so.userPrivateKey, "S0huLfhqH+046Hy9UYAY6r4N2/P7Hbeq94FyAoQSlxuA=");
         assertEquals(so.clusterPublicKey, "Pm6Y4ggcD1FhFAviA6EQSUtv/5qjT/IFIdrKNeCJtiyM=");
     }
-
-    @Test
-    public void canReadComplexPaths () throws IOException {
-        Session.SecurityOptions so =
-            Session.SecurityOptions.ofFiles("../qdb-api-jni/test_private.key",
-                                            "../qdb-api-jni/test_public.key");
-
-        assertEquals(so.userName, "test-user");
-        assertEquals(so.userPrivateKey, "S0huLfhqH+046Hy9UYAY6r4N2/P7Hbeq94FyAoQSlxuA=");
-        assertEquals(so.clusterPublicKey, "Pm6Y4ggcD1FhFAviA6EQSUtv/5qjT/IFIdrKNeCJtiyM=");
-    }
 }
