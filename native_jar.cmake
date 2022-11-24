c65ea3e0761c23f35522757c0d6d5766b9db748f
Leon Mergen <leon@solatis.com>
2022-11-24 14:56:55 +0700

    Bump version to 3.13.6.

--- native_jar.cmake
+++ native_jar.cmake
@@ -10,7 +10,7 @@ else()
     string(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM)
 endif()
 
-set(NATIVE_JAR_FILE "${CMAKE_SOURCE_DIR}/target/jni-3.13.6-SNAPSHOT-${SYSTEM}-${ARCH}.jar")
+set(NATIVE_JAR_FILE "${CMAKE_SOURCE_DIR}/target/jni-3.13.6-${SYSTEM}-${ARCH}.jar")
 
 # Quasardb C API
 if (WIN32)
