e860cf1d97f7d1e8698a1e9d72402740fb81d697
Leon Mergen <leon@solatis.com>
2024-09-10 17:15:42 +0700

    Fix perf trace tests

--- src/test/java/net/quasardb/qdb/PerformanceTraceTest.java
+++ src/test/java/net/quasardb/qdb/PerformanceTraceTest.java
@@ -61,7 +61,7 @@ public class PerformanceTraceTest {
         Table t = TestUtils.createTable(s, columns);
 
         Collection<PerformanceTrace.Trace> res = PerformanceTrace.get(s);
-        assertEquals(3, res.size());
+        assertEquals(2, res.size());
 
         for (PerformanceTrace.Trace trace : res) {
             assertTrue(trace.measurements.length > 1);
@@ -89,7 +89,7 @@ public class PerformanceTraceTest {
         PerformanceTrace.clear(s);
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.get(s);
 
-        assertEquals(3, res1.size());
+        assertEquals(2, res1.size());
         assertEquals(0, res2.size());
     }
 
@@ -105,7 +105,7 @@ public class PerformanceTraceTest {
         Collection<PerformanceTrace.Trace> res1 = PerformanceTrace.pop(s);
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.get(s);
 
-        assertEquals(3, res1.size());
+        assertEquals(2, res1.size());
         assertEquals(0, res2.size());
     }
 
@@ -124,7 +124,7 @@ public class PerformanceTraceTest {
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.pop(s);
 
         assertEquals(0, res1.size());
-        assertEquals(3, res2.size());
+        assertEquals(2, res2.size());
 
         Writer w = Writer.builder(s).build();
 
