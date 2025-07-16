17aa3dc72058b32bd37e9d7d92762053181f427f
Igor <igor.niebylski@gmail.com>
2025-07-16 08:11:46 +0200

    will this work?

--- src/test/java/net/quasardb/qdb/PerformanceTraceTest.java
+++ src/test/java/net/quasardb/qdb/PerformanceTraceTest.java
@@ -70,7 +70,7 @@ public class PerformanceTraceTest {
         Table t = TestUtils.createTable(s, columns);
 
         Collection<PerformanceTrace.Trace> res = PerformanceTrace.get(s);
-        assertEquals(2, res.size());
+        assertEquals(3, res.size());
 
         for (PerformanceTrace.Trace trace : res) {
             assertTrue(trace.measurements.length > 1);
@@ -96,7 +96,7 @@ public class PerformanceTraceTest {
         PerformanceTrace.clear(s);
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.get(s);
 
-        assertEquals(2, res1.size());
+        assertEquals(3, res1.size());
         assertEquals(0, res2.size());
     }
 
@@ -110,7 +110,10 @@ public class PerformanceTraceTest {
         Collection<PerformanceTrace.Trace> res1 = PerformanceTrace.pop(s);
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.get(s);
 
-        assertEquals(2, res1.size());
+        System.out.println("res1: " + res1);
+        System.out.println("res2: " + res2);
+
+        assertEquals(3, res1.size());
         assertEquals(0, res2.size());
     }
 
@@ -127,7 +130,7 @@ public class PerformanceTraceTest {
         Collection<PerformanceTrace.Trace> res2 = PerformanceTrace.pop(s);
 
         assertEquals(0, res1.size());
-        assertEquals(2, res2.size());
+        assertEquals(3, res2.size());
 
         Writer w = Writer.builder(s).build();
 
@@ -145,7 +148,7 @@ public class PerformanceTraceTest {
         w.flush();
 
         Collection<PerformanceTrace.Trace> res5 = PerformanceTrace.pop(s);
-        assertEquals(2, res5.size());
+        assertEquals(3, res5.size());
     }
 
     @Test
